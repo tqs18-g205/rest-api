6438502bf394f6871712ef2c473e0524b308a704
tkardozo <t.cardoso@ua.pt>
2018-05-07 19:19:25 +0100

    New Test with Assert Error

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tkardozo
+ */
+public class HelloTest {
+
+    
+    public static void main(String[] args) {
+        assert false;
+    }
+    
+}
