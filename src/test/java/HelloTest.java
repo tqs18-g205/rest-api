81e7538b4110d03f68071e2f222181fbe04859e4
tkardozo <t.cardoso@ua.pt>
2018-05-07 19:22:39 +0100

    New Test with a true Assert Error

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,3 +1,6 @@
+
+import org.junit.Test;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +13,8 @@
  */
 public class HelloTest {
 
-    
-    public static void main(String[] args) {
+    @Test
+    void testThis() {
         assert false;
     }
     
