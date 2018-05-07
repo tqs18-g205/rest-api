25aab35aba0f41389986996f2f13fe62d5a6310e
tkardozo <t.cardoso@ua.pt>
2018-05-07 23:06:51 +0100

    Testing Coding Styles

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 
 public class HelloWorld {
 
-   private static final Logger LOGGER = Logger.getLogger(
+  private static final Logger LOGGER = Logger.getLogger(
       Thread.currentThread().getStackTrace()[0].getClassName() );
     
   public static void main(String[] args) {
