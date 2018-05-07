f0fcc1234bc5534a84af007e636dbb25efdc45df
tkardozo <t.cardoso@ua.pt>
2018-05-07 20:32:44 +0100

    New Test Over Again

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,11 +2,16 @@
  *
  * @author tkardozo
  */
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
+    private static final Logger LOGGER = Logger.getLogger(
+        Thread.currentThread().getStackTrace()[0].getClassName() );
+    
     public static void main(String[] args) {
         //AHAHAHAH
-	System.out.print("GoNutri FTW");
+	LOGGER.info("GoNutri FTW");
     }
     
 }
